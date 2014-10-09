c9dbc8bd1a7d5c802cfa45154bfa5bd4c53b9668
Howard Wilson <howard@watsonbox.net>
2014-10-09 12:21:45 +0200

    Add basic xbmc control to listener.rb using xbmc-client

---
+++ Gemfile
@@ -0,0 +1,3 @@
+source 'https://rubygems.org'
+
+gem "xbmc-client", :git => "git@github.com:dr-impossible/xbmc-client.git"
--- listener.rb
+++ listener.rb
@@ -1,3 +1,5 @@
+require 'xbmc-client'
+
 import "java.util.HashMap"
 import "java.util.Map"
 
@@ -6,47 +8,67 @@ $CLASSPATH << "sphinx4/sphinx4-core/target/classes"
 import "edu.cmu.sphinx.api.Configuration"
 import "edu.cmu.sphinx.api.LiveSpeechRecognizer"
 
+DATA_RESOURCES_DIR = "./sphinx4/sphinx4-data/src/main/resources"
+ACOUSTIC_MODEL_PATH = File.join(DATA_RESOURCES_DIR, "edu/cmu/sphinx/models/acoustic/wsj")
+DICTIONARY_PATH = File.join(DATA_RESOURCES_DIR, "edu/cmu/sphinx/models/acoustic/wsj/dict/cmudict.0.6d")
+GRAMMAR_PATH = "./grammars/"
+
 class Listener
-  DATA_RESOURCES_DIR = "./sphinx4/sphinx4-data/src/main/resources"
-  DEMO_RESOURCES_DIR = "./sphinx4/sphinx4-samples/src/main/resources"
-  ACOUSTIC_MODEL = File.join(DATA_RESOURCES_DIR, "edu/cmu/sphinx/models/acoustic/wsj")
-  DICTIONARY_PATH = File.join(DATA_RESOURCES_DIR, "edu/cmu/sphinx/models/acoustic/wsj/dict/cmudict.0.6d")
-  GRAMMAR_PATH = File.join(DEMO_RESOURCES_DIR, "edu/cmu/sphinx/demo/dialog/")
-  LANGUAGE_MODEL = File.join(DEMO_RESOURCES_DIR, "edu/cmu/sphinx/demo/dialog/weather.lm")
+  attr_accessor :configuration
+
+  def initialize(acoustic_model_path, dictionary_path, grammar_path)
+    self.configuration = Configuration.new
+
+    configuration.setAcousticModelPath(acoustic_model_path)
+    configuration.setDictionaryPath(dictionary_path)
+    configuration.setGrammarPath(grammar_path)
+    configuration.setUseGrammar(true)
+    configuration.setGrammarName("xbmc")
+  end
 
   def listen
-    configuration = Configuration.new
-    configuration.setAcousticModelPath(ACOUSTIC_MODEL)
-    configuration.setDictionaryPath(DICTIONARY_PATH)
-    configuration.setGrammarPath(GRAMMAR_PATH)
-    configuration.setUseGrammar(true)
+    jsgfRecognizer = LiveSpeechRecognizer.new(configuration).tap do |recognizer|
+      recognizer.startRecognition(true)
+    end
 
-    configuration.setGrammarName("dialog")
-    jsgfRecognizer = LiveSpeechRecognizer.new(configuration)
-
-    configuration.setGrammarName("digits.grxml")
-    grxmlRecognizer = LiveSpeechRecognizer.new(configuration)
-
-    configuration.setUseGrammar(false)
-    configuration.setLanguageModelPath(LANGUAGE_MODEL)
-    lmRecognizer = LiveSpeechRecognizer.new(configuration)
-
-    jsgfRecognizer.startRecognition(true)
+    say "Listening"
 
     while true do
       puts "Listening..."
 
-      utterance = jsgfRecognizer.getResult().getHypothesis()
+      puts utterance = jsgfRecognizer.getResult().getHypothesis()
 
       if (utterance.start_with?("exit"))
         break
-      end
-
-      if (utterance.end_with?("weather forecast"))
-        puts "Weather forecast"
+      elsif (utterance.start_with?('move'))
+        kodi.input.send utterance.split.last
+      elsif (utterance.start_with?('select'))
+        kodi.input.select
+      elsif (utterance.start_with?('back'))
+        kodi.input.back
       end
     end
   end
+
+  private
+
+  # OSX specific implementation for now
+  def say(text)
+    `say #{text}`
+  end
+
+  # Wrap xbmc-client for nicer API
+  def kodi
+    return @kodi if @kodi
+
+    Xbmc.base_uri "http://localhost:8080"
+    Xbmc.basic_auth "xbmc", "xbmc"
+    Xbmc.load_api!
+
+    @kodi = Struct.new(:input).new(Xbmc::Input)
+  end
 end
 
-Listener.new.listen
+listener = Listener.new ACOUSTIC_MODEL_PATH, DICTIONARY_PATH, GRAMMAR_PATH
+
+listener.listen
